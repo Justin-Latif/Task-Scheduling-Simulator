c35ee2a0b1e31dbefc178708a4d371a31ccffd1a
Justin Latif <75379075+Justin-Latif@users.noreply.github.com>
2025-10-23 23:29:36 -0400

    Refactor ECSimTask3.h with new task classes

--- Project/part-II/ECSimTask3.h
+++ Project/part-II/ECSimTask3.h
@@ -100,51 +100,103 @@ private:
 // Consecutive task: a task that can early abort
 // This class modifies the passed-in task to have the consecutive property
 
-class ECSimConsecutiveTask 
+class ECSimConsecutiveTask : public ECSimTask
 {
 public:
   ECSimConsecutiveTask(ECSimTask *pTask);
   
   // your code ehre 
-    
+  virtual std::string GetId() const {return decorator->GetId();} // Virtual overrides for methods for cpp with implementation
+  virtual bool IsReadyToRun(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsFinished(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsAborted(int tick) const; // Virtual overrides for methods for cpp
+  virtual void Run(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual void Wait(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual int GetTotWaitTime() const {return decorator->GetTotWaitTime();} // Virtual overrides for methods for cpp with implementation
+  virtual int GetTotRunTime() const {return decorator->GetTotRunTime();} // Virtual overrides for methods for cpp with implementation
+
+private:
+  ECSimTask* decorator; // Decorator pointer
+  bool interrupted; // Represents whether or not the task was interrupted, causing immediate shutdown of the task since consecutive
 };
 
 //***********************************************************
 // Periodic task: a task that can early abort
 // This class modifies the passed-in task to have the early abort property
 
-class ECSimPeriodicTask 
+class ECSimPeriodicTask : public ECSimTask
 {
 public:
   ECSimPeriodicTask(ECSimTask *pTask, int lenSleep);
 
   // your code here
-    
+  virtual std::string GetId() const {return decorator->GetId();} // Virtual overrides for methods for cpp with implementation
+  virtual bool IsReadyToRun(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsFinished(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsAborted(int tick) const {return decorator->IsAborted(tick);} // Virtual overrides for methods for cpp with implementation
+  virtual void Run(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual void Wait(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual int GetTotWaitTime() const {return decorator->GetTotWaitTime();} // Virtual overrides for methods for cpp with implementation
+  virtual int GetTotRunTime() const {return decorator->GetTotRunTime();} // Virtual overrides for methods for cpp with implementation
+
+private:
+  ECSimTask* decorator; // Decorator pointer
+  int lenSleep; // Copy of the lenSleep constructor variable that represents the length of sleep in between tasks
+  mutable int currentStart; // Represents the last tick's start time, all has to be mutable or errors generate
+  mutable int currentEnd; // Represents the last tick's end time, all has to be mutable or errors generate
+  mutable int nextStart; // Represents the next tick's start time, all has to be mutable or errors generate
+  mutable int nextEnd; // Represents the next tick's end time, all has to be mutable or errors generate
+  mutable bool started; // Represents whether or  not the tasks are running, all has to be mutable or errors generate
+  mutable bool interrupted; // Represents if the task was interrupted or waited, all has to be mutable or errors generate
 };
 
 //***********************************************************
 // Task with a deadline to start: a task that must start by some time; otherwise terminate
 // This class modifies the passed-in task to have a deadline to start 
 
-class ECSimStartDeadlineTask 
+class ECSimStartDeadlineTask : public ECSimTask
 {
 public:
   ECSimStartDeadlineTask(ECSimTask *pTask, int tmStartDeadline);
 
   // your code here
+  virtual std::string GetId() const {return decorator->GetId();} // Virtual overrides for methods for cpp with implementation
+  virtual bool IsReadyToRun(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsFinished(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsAborted(int tick) const; // Virtual overrides for methods for cpp
+  virtual void Run(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual void Wait(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual int GetTotWaitTime() const {return decorator->GetTotWaitTime();} // Virtual overrides for methods for cpp with implementation
+  virtual int GetTotRunTime() const {return decorator->GetTotRunTime();} // Virtual overrides for methods for cpp with implementation
+
+private:
+  ECSimTask* decorator; // Decorator pointer
+  bool started; // Started variable to mark whether it has started or not for checks
+  int deadline; // Specific time that the deadline is marked on, from the constructor variable tmStartDeadline
 };
 
 //***********************************************************
 // Task must end by some fixed time click: this is useful e.g. when a task is periodic
 // This class modifies the passed-in task to have a deadline to end 
 
-class ECSimEndDeadlineTask 
+class ECSimEndDeadlineTask : public ECSimTask
 {
 public:
   ECSimEndDeadlineTask(ECSimTask *pTask, int tmEndDeadline);
 
   // your code here
-    
+  virtual std::string GetId() const {return decorator->GetId();} // Virtual overrides for methods for cpp with implementation
+  virtual bool IsReadyToRun(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsFinished(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsAborted(int tick) const; // Virtual overrides for methods for cpp
+  virtual void Run(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual void Wait(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual int GetTotWaitTime() const {return decorator->GetTotWaitTime();} // Virtual overrides for methods for cpp with implementation
+  virtual int GetTotRunTime() const {return decorator->GetTotRunTime();} // Virtual overrides for methods for cpp with implementation
+
+private:
+  ECSimTask* decorator; // Decorator pointer
+  int deadline; // Specific time that the deadline is marked on, from the constructor variable tmEndDeadLine
 };
 
 //***********************************************************
@@ -155,13 +207,25 @@ class ECSimCompositeTask : public ECSimTask
 public:
   ECSimCompositeTask(const std::string &tidIn);
     
-  virtual std::string GetId() const;
+  virtual std::string GetId() const{return tid;}
     
   // Add subtask
   void AddSubtask(ECSimTask *pt);
 
   // your code ehre
-    
+  virtual bool IsReadyToRun(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsFinished(int tick) const; // Virtual overrides for methods for cpp
+  virtual bool IsAborted(int tick) const; // Virtual overrides for methods for cpp
+  virtual void Run(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual void Wait(int tick, int duration); // Virtual overrides for methods for cpp
+  virtual int GetTotWaitTime() const; // Virtual overrides for methods for cpp
+  virtual int GetTotRunTime() const; // Virtual overrides for methods for cpp
+
+private:
+  std::string tid; // For the id variable in the constructor
+  std::vector<ECSimTask*> list; // List to represent the composite tasks altogether for iteration
+  int runtime; // Runtime counter instead of using the normal GetTotRunTime to compile all tasks together
+  int waittime; // Waittime counter instead of using the normal GetTotWaitTime to compile all tasks together
 };
 
 #endif /* ECSimTask3_h */
